fba332f2601ae312b27d8b6bb2524b1145710d05
patricio Daniel <pato.rubbo@gmail.com>
2024-08-21 12:44:55 -0300

    Agregar archivos de proyecto.

---
+++ EjemploClase/Controllers/PersonaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EjemploClase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        // POST api/<PersonaController>
+        [HttpPost]
+        public List<Persona> GetPersonas(Persona persona)
+        { List <Persona> lista = new List<Persona>();
+
+            for (int i = 0;i<10;i++)
+            {
+                lista.Add(persona);
+            }
+            return lista;
+
+        }
+    }
+}
---
+++ EjemploClase/Persona.cs
@@ -0,0 +1,12 @@
+﻿namespace EjemploClase
+{
+    public class Persona
+    {
+        public string nombre {  get; set; }
+        public string apellido { get; set; }
+        public string edad { get; set; }
+        public string profesion { get; set; }
+
+
+    }
+}
